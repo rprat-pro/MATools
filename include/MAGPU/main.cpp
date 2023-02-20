f4517757c49704832a07b86ebaee1a56a1af0f91
PRAT Raphael <rp269144@pleiades1217.intra.cea.fr>
2023-02-20 16:11:11 +0100

    check get_data in MAGPURunner

--- include/MAGPU/main.cpp
+++ include/MAGPU/main.cpp
@@ -12,26 +12,27 @@ bool run_test_runner()
 	MAGPUVector<T, MODE, GT> res;
 	MAGPUVector<T, MODE, GT> vec1;
 	MAGPUVector<T, MODE, GT> vec2;
+	T val = 13;
 
 	res.init(0.0, N);
 	vec1.init(1.0, N);
 	vec2.init(2.0, N);
 
-	auto add_kernel = [](unsigned int idx, double* const out, double* const in1, double* const in2)
+	auto add_kernel = [](unsigned int idx, T* const out, T* const in1, T* const in2, T val)
 	{
-		out[idx] = in1[idx] + in2[idx];
+		out[idx] = in1[idx] + in2[idx] + val;
 	};
 
 	MAGPUFunctor<decltype(add_kernel), GPU_TYPE::SERIAL> fun(add_kernel, "add");
 	MAGPURunner<MODE, GT> runner;
-	runner(fun, N, res, vec1, vec2);	
+	runner(fun, N, res, vec1, vec2, val);	
 
 	// check
 	std::vector<T> host = res.copy_to_vector();
 
 	for(int id = 0 ; id < N ; id++)
 	{
-		if(host[id] != 3.0)
+		if(host[id] != 3.0 + val)
 		{
 			std::cout << "MATools_LOG: id = "<<id << " and host[id] = " << host[id] << std::endl;
 			std::cout << "MATools_LOG: Error in test runner " << std::endl;
