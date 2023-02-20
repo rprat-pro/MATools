2e042a289c1476012fa4e77de722c11ba2102cc0
PRAT Raphael <rp269144@pleiades1217.intra.cea.fr>
2023-02-20 15:52:05 +0100

    fix test

--- include/MAGPU/main.cpp
+++ include/MAGPU/main.cpp
@@ -3,52 +3,51 @@
 #include <MAGPUFunctor.hxx>
 #include <MAGPURunner.hxx>
 
-template<typename T, MATools::MAGPU::MEM_MODE MODE, MATools::MAGPU::GPU_TYPE GT>
+	template<typename T, MATools::MAGPU::MEM_MODE MODE, MATools::MAGPU::GPU_TYPE GT>
 bool run_test_runner()
 {
-  using namespace MATools::MAGPU;
-  constexpr int N = 1000;
+	using namespace MATools::MAGPU;
+	constexpr int N = 1000;
 
-  MAGPUVector<T,MODE,GT> res;
-  MAGPUVector<T,MODE,GT> vec1;
-  MAGPUVector<T,MODE,GT> vec2;
+	MAGPUVector<T, MODE, GT> res;
+	MAGPUVector<T, MODE, GT> vec1;
+	MAGPUVector<T, MODE, GT> vec2;
 
-  res.init(0.0, N);
-  vec1.init(1.0, N);
-  vec2.init(2.0, N);
+	res.init(0.0, N);
+	vec1.init(1.0, N);
+	vec2.init(2.0, N);
 
 	auto add_kernel = [](unsigned int idx, double* const out, double* const in1, double* const in2)
 	{
 		out[idx] = in1[idx] + in2[idx];
 	};
-	
+
 	MAGPUFunctor<decltype(add_kernel), GPU_TYPE::SERIAL> fun(add_kernel, "add");
-	MAGPURunner<MEM_MODE::CPU, GPU_TYPE::SERIAL> runner;
+	MAGPURunner<MODE, GT> runner;
 	runner(fun, N, res, vec1, vec2);	
 
+	// check
+	std::vector<T> host = res.copy_to_vector();
 
-  // check
-  std::vector<T> host = res.copy_to_vector();
+	for(int id = 0 ; id < N ; id++)
+	{
+		if(host[id] != 3.0)
+		{
+			std::cout << "MATools_LOG: id = "<<id << " and host[id] = " << host[id] << std::endl;
+			std::cout << "MATools_LOG: Error in test runner " << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
 
-  for(int id = 0 ; id < N ; id++)
-  {
-    if(host[id] != 3.0)
-    {
-      std::cout << "MATools_LOG: id = "<<id << " and host[id] = " << host[id] << std::endl;
-      std::cout << "MATools_LOG: Error in test runner " << std::endl;
-      return EXIT_FAILURE;
-    }
-  }
-
-  std::cout << "MATools_LOG: test runner works correctly " << std::endl;
-  return EXIT_SUCCESS;
+	std::cout << "MATools_LOG: test runner works correctly " << std::endl;
+	return EXIT_SUCCESS;
 }
 
 int main()
 {
-  bool success = EXIT_SUCCESS;
-  using namespace MATools::MAGPU;
-  success &= run_test_runner<double, MEM_MODE::CPU, GPU_TYPE::SERIAL>();
-  success &= run_test_runner<double, MEM_MODE::GPU, GPU_TYPE::SERIAL>();
-  return success;
+	bool success = EXIT_SUCCESS;
+	using namespace MATools::MAGPU;
+	success &= run_test_runner<double, MEM_MODE::CPU, GPU_TYPE::SERIAL>();
+	success &= run_test_runner<double, MEM_MODE::GPU, GPU_TYPE::SERIAL>();
+	return success;
 }
